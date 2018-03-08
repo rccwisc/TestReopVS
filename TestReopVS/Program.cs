18e76abfbb06bb3fa74ec1f7fccd437eed5f07df
rccwisc <rccwisc@gmail.com>
2018-03-08 15:57:58 -0600

    palindome numbers or strings

--- TestReopVS/Program.cs
+++ TestReopVS/Program.cs
@@ -10,7 +10,24 @@ namespace TestReopVS
     {
         static void Main(string[] args)
         {
-            Console.Write("hello GIT world");
+            Console.Write(value: "Enter palindrome");
+            var input = Console.ReadLine();
+            Console.WriteLine("{0} is typeof {1}", input, input.GetType());
+            string reverse = Reverse(input);
+            if ( input.Equals(reverse) )
+            {
+                Console.WriteLine("yep palindrome");
+            } else
+            {
+                Console.WriteLine("nope not a palindrome {0}.NotEquals({1})",input,reverse);
+            }
+        }
+
+        public static string Reverse(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
         }
     }
 }
